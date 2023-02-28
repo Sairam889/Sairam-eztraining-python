74092cacedb268821113f5874213dd7f0be391d1
Sairam889 <124248123+Sairam889@users.noreply.github.com>
2023-02-28 10:24:22 +0530

    Add files via upload

---
+++ Day 3 program.py
@@ -0,0 +1,30 @@
+#Prog to print the given list one by one
+L=list(map(int,input("Enter 10 elements:").split(",")))
+for i in range(len(L)):
+           print(L[i],end=" ")
+           i=i+1
+
+#prog to print the sum and avg of 5 floating numbers
+L=list(map(float,input("Enter 5 floating point numbers:").split(",")))
+sum=0
+for i in range(len(L)):
+           sum=sum+L[i]
+           i=i+1
+print("Sum=",sum)
+print("Average=",(sum/len(L)))
+
+#prog to print the even numbers in the list
+L=list(map(int,input("Enter 6 elements:").split(",")))
+for i in range(len(L)):
+    if(L[i]%2==0):
+        print(L[i],end=" ")
+    else:
+        pass
+
+#prog to print the even numbers in the list taking n inputs 
+L=list(map(int,input("Enter the elements:").split(",")))
+for i in range(len(L)):
+    if(L[i]%2==0):
+        print(L[i],end=" ")
+    else:
+        pass
